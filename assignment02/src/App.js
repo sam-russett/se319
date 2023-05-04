15b6d52297153ab47da4d68f9e789c9a515dc1f1
jasond14 <jasondcruz36@gmail.com>
2023-05-04 18:47:45 -0500

    import final project base files

--- assignment02/src/App.js
+++ assignment02/src/App.js
@@ -2,14 +2,17 @@ import "./App.css";
 import items from "./products"
 import React, { useState, useEffect } from "react";
 
+
+
+
 export const App = () => {
-  console.log("Step 1: After reading file :");
-  
+
   const [ProductsCategory, setProductsCategory] = useState(items);
   // var ProductsCategory = Products;
   const [cart, setCart] = useState([]);
   const [cartTotal, setCartTotal] = useState(0);
   const [query, setQuery] = useState('');
+  const [showCheckout, setShowCheckout] = useState(false);
     useEffect(() => {total();}, [cart]);
     
     const total = () => {
@@ -19,6 +22,7 @@ export const App = () => {
         }
         setCartTotal(totalVal);
     };
+
     const addToCart = (el) => {
         setCart([...cart, el]);
     };
@@ -54,22 +58,32 @@ export const App = () => {
     setProductsCategory(results);
   }
     
+  function handleClickCheckout() {
+    setShowCheckout(!showCheckout);
+  }
 
   function howManyofThis(id) {
     let hmot = cart.filter((cartItem) => cartItem.id === id);
     return hmot.length;
-}
+  }
 
   const render_products = (ProductsCategory) => {
 
     return <div className='category-section fixed'>
 	  {console.log("Step 3 : in render_products ")}
       <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">Products ({ProductsCategory.length})</h2>
+      <button
+          class="relative bg-blue-500 text-white p-3 rounded font-bold overflow-visible"
+          type="button"
+          onClick={() => handleClickCheckout()}
+        >
+          <h2>View My Cart</h2>
+      </button>
       <div className="py-10">
         <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="search" value={query} onChange={handleChange} />
       </div>
 
-      <div className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10" style={{ maxHeight: '700px', overflowY: 'scroll' }}>
+      <div className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10" style={{ maxHeight: '550px', overflowY: 'scroll' }}>
         {/* Loop Products */}
         {ProductsCategory.map((product, index) => (
           <div key={index} className="group relative shadow-lg" >
@@ -99,93 +113,33 @@ export const App = () => {
             </div>
           </div>
         ))}
+        {showCheckout && (
+            <div>
+              STORE SE/ComS319
+              <div class="card">
+                <div class="row">
+                  <div class="float-end">
+                    <p class="mb-0 me-5 d-flex align-items-center">
+                      {cartItems}
+                      <span class="small text-muted me-2">Order total:</span>
+                      <span class="lead fw-normal">${cartTotal}</span>
+                      
+                    </p>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
       </div>
     </div>
   }
-
-  const Shop = () => {
-    // const [cart, setCart] = useState([]);
-    // const [cartTotal, setCartTotal] = useState(0);
-    // useEffect(() => {total();}, [cart]);
-    
-    // const total = () => {
-    //     let totalVal = 0;
-    //     for (let i = 0; i < cart.length; i++) {
-    //         totalVal += cart[i].price;
-    //     }
-    //     setCartTotal(totalVal);
-    // };
-    // const addToCart = (el) => {
-    //     setCart([...cart, el]);
-    // };
-
-    // const removeFromCart = (el) => {
-    //     let hardCopy = [...cart];
-    //     hardCopy = hardCopy.filter((cartItem) => cartItem.id !== el.id);
-    //     setCart(hardCopy);
-    // };
-
-    // const cartItems = cart.map((el) => (
-    //     <div key={el.id}>
-    //         <img class="img-fluid" src={el.image} width={30} />
-    //         <p>{el.title} ${el.price}</p>
-    //     </div>
-    // ));
-    
-    // const listItems = Products.map((el) => (
-    //     <div key={el.id}>
-    //         <img class="img-fluid" src={el.image} width={100} />
-    //         <p>{el.title} 
-    //         {el.category} 
-    //         {el.price}
-    //         </p> 
-    //         <button type="button" onClick={() => removeFromCart(el)}>-</button>{" "}
-    //         <button type="button" variant="light" onClick={() => addToCart(el)}> + </button>
-    //     </div>
-    // ));
-
-    // return (
-    //     <div>
-    //         <div> {listItems} </div>
-    //         <div>Items in Cart :</div>
-    //         <div>{cartItems}</div>
-    //         <div>Order total to pay: {cartTotal}</div>
-    //     </div>
-    // );
-}
-  
-  // function handleClick(tag){
-  //   console.log("Step 4 : in handleClick", tag);
-  //   let filtered = Products.filter(cat => cat.category === tag);
-  //   setProductsCategory(filtered);
-  //   // ProductsCategory = filtered;
-  //   console.log("Step 5 : ", Products.length, ProductsCategory.length);
-  // }
-
   
   return (
-    // <div className="flex fixed flex-row">
-	  //   {console.log("Step 2 : Return App :",Products.length,ProductsCategory.length)}
-    //   <div className="h-screen  bg-slate-800 p-3 xl:basis-1/5" style={{ minWidth: '65%' }}>
-    //     <img className="w-full" src={logo} alt="Sunset in the mountains" />
-    //     <div className="px-6 py-4">
-    //       <h1 className="text-3xl mb-2 font-bold text-white"> Product Catalog App </h1>
-    //       <p className="text-gray-700 text-white">
-    //         by - <b style={{ color: 'orange' }}>Jason Delacruz, Sam Russett</b>
-    //       </p>
-    //       <div className="py-10">
-    //         { (Categories) ? <p className='text-white'>Tags : </p> : ''}
-    //         {
-    //           Categories.map(tag => <button key={tag} className="inline-block bg-amber-600 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mt-2" onClick={()=>{handleClick(tag)}}>{tag}</button>)
-    //         }
-    //       </div>
-    //     </div>
-    //   </div>
       <div className="ml-5  p-10 xl:basis-4/5">
         {console.log("Before render :",items.length,ProductsCategory.length)}
         {render_products(ProductsCategory)}
       </div>
-    // </div>
   );
-
 };
+
